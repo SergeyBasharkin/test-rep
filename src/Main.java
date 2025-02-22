facd47cc7e4b0916e6669d3553659a4edece3f3d
Sergei Basharkin <Basharkin@lanit.ru>
2025-02-22 11:16:17 +0300

    test PR

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        int a = 51;
+        int a = 5231;
         double fg = 5.0;
 
         System.out.println(a + fg); // Outputs: 56.0
